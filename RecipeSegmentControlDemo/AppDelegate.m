c491a69896547f554210afbd1c5f0a77c0d0cf01
Derek Yang <yanghada@gmail.com>
2012-05-31 18:39:12 +0200

    Updated copyright.

--- RecipeSegmentControlDemo/AppDelegate.m
+++ RecipeSegmentControlDemo/AppDelegate.m
@@ -3,7 +3,7 @@
 //  RecipeSegmentControlDemo
 //
 //  Created by Derek Yang on 05/30/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Derek Yang. All rights reserved.
 //
 
 #import "AppDelegate.h"
